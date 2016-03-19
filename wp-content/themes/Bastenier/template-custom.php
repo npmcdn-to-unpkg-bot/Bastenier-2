3f5d230badba42f7bc503493ac68dfa050ecfbf2
Peter Vanwyck <pvanwyck@prophets.be>
2016-03-19 21:59:35 +0100

    Aanvang news overview

--- wp-content/themes/Bastenier/template-custom.php
+++ wp-content/themes/Bastenier/template-custom.php
@@ -9,3 +9,31 @@
   <?php get_template_part('templates/content', 'page'); ?>
   <?php get_template_part('templates/promoted'); ?>
 <?php endwhile; ?>
+
+<?php
+  $page_for_posts_id = get_option( 'page_for_posts' );
+  $page_for_posts = get_post ($page_for_posts_id);
+  if($page_for_posts != NULL) {
+    echo '<h2>'.$page_for_posts->post_title.'</h2>';
+  }
+?>
+
+<?php $args = array(
+  'numberposts' => 1,
+  'orderby' => 'post_date',
+  'order' => 'DESC',
+  'post_type' => 'post',
+  'post_status' => 'publish',
+  'suppress_filters' => true );
+
+  $recent_posts = get_posts( $args);
+?>
+
+<div class="post-overview">
+    <?php
+      foreach ( $recent_posts as $post ){
+        setup_postdata( $post );
+        get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format());
+      }
+    ?>
+</div>
--- wp-content/themes/Bastenier/templates/content.php
+++ wp-content/themes/Bastenier/templates/content.php
@@ -1,9 +1,24 @@
 <article <?php post_class(); ?>>
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php get_template_part('templates/entry-meta'); ?>
-  </header>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
+  <div class="post__image">
+
+    <?php
+      if(has_post_thumbnail ()){
+        the_post_thumbnail('medium');
+      } else {
+        $fallbackImageID = get_option( 'bastenier_post_fallback_image', false )[0];
+        echo wp_get_attachment_image ( $fallbackImageID, 'large');
+      }
+    ?>
+  </div>
+  <div class="post__body">
+    <header>
+      <div class="post__meta">
+        <?php get_template_part('templates/entry-meta'); ?>
+      </div>
+      <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    </header>
+    <div class="entry-summary">
+      <?php the_excerpt(); ?>
+    </div>
   </div>
 </article>
