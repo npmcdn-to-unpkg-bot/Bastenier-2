b0c79e4bf84f08c30f56d98fe46467ced013fbc5
Peter Vanwyck <pvanwyck@prophets.be>
2016-03-29 09:31:53 +0200

    footer

--- wp-content/themes/Bastenier/templates/footer.php
+++ wp-content/themes/Bastenier/templates/footer.php
@@ -1,16 +1,16 @@
 <footer class="footer">
   <div class="footer__body">
-    <div class="footer__contact">
-      <?php echo get_option( 'bastenier_footer_left', false );?>
-    </div>
     <div class="footer__logo">
       <img src="<?php echo get_template_directory_uri(); ?>/dist/images/logo--small.svg" alt="Bastenier" />
     </div>
+    <div class="footer__contact">
+      <?php echo get_option( 'bastenier_footer_left', false );?>
+    </div>
     <div class="footer__newsletter">
-      [ontvang onze nieuwsbrief]
       <form>
-        <input type="email" placeholder="[uw emailadres hier]"/>
-        <button type="submit">[verstuur]</button>
+        <label for="email">[ontvang <strong>onze nieuwsbrief: </strong>]</label>
+        <input type="email" id="email" class="no-space" placeholder="[uw emailadres hier]"/>
+        <input type="submit" class="no-space" value="[verstuur]"/></button>
       </form>
       <div class="footer__social">
       </div>
