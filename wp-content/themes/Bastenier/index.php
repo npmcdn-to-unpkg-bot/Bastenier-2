e31a648b07a2f150bf311314f0e525d3b205e536
Peter Vanwyck <pvanwyck@prophets.be>
2016-05-05 12:39:35 +0200

    rework wp_query for custom taxonomy filtering

--- wp-content/themes/Bastenier/index.php
+++ wp-content/themes/Bastenier/index.php
@@ -7,11 +7,46 @@
   <?php get_search_form(); ?>
 <?php endif; ?>
 
+<?php
+
+  $args = array(
+    'post_type' => array( 'post'),
+    'post_status' => 'publish',
+    'tax_query' => array(
+        array(
+            'taxonomy' => 'rechtstak',
+            'field' => 'name',
+            'terms' => 'verkeer'
+        )
+    ),
+    'orderby' => 'modified',
+  );
+
+  $args['paged'] = get_query_var( 'paged' )
+      ? get_query_var( 'paged' )
+      : 1;
+
+  $custom_query = new WP_Query($args);
+  $temp_query = $wp_query;
+  $wp_query   = NULL;
+  $wp_query   = $custom_query;
+?>
+
 <div class="post-overview">
-  this is the post overview
-  <?php while (have_posts()) : the_post(); ?>
-    <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-  <?php endwhile; ?>
+  <?php
+    while (have_posts()) :
+      the_post();
+      get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format());
+    endwhile;
+
+    wp_reset_postdata(); // reset the query
+
+    the_posts_navigation(); // navigation
+
+    //reset wp_query
+    $wp_query = NULL;
+    $wp_query = $temp_query;
+  ?>
 </div>
 
-<?php the_posts_navigation(); ?>
+<?php  ?>
