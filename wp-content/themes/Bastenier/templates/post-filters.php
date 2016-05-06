a9810cff8b58d412363d32e469a86ee34976cee7
Peter Vanwyck <pvanwyck@prophets.be>
2016-05-06 11:53:27 +0200

    add filters html

--- wp-content/themes/Bastenier/templates/post-filters.php
+++ wp-content/themes/Bastenier/templates/post-filters.php
@@ -1,3 +1,21 @@
+<?php
+
+  $terms = get_terms( array(
+      'taxonomy' => 'rechtstak',
+      'hide_empty' => false,
+  ) );
+
+
+?>
 <div class="post-filters">
-filters
+  <ul>
+    <?php
+      foreach ($terms  as $term ) {
+          $querystring = '?taxonomy='.$term->slug;
+          $url = get_permalink(get_option( 'page_for_posts' ));
+
+          echo '<li> <a href="'. $url.$querystring .'">' . $term->name . '</a></li>';
+        }
+    ?>
+  </ul>
 </div>
